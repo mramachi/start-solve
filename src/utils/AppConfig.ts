990c60a089f46360b42e684838e2a80260fc1d64
mramachi <moustapha.ramachi@gmail.com>
2024-10-13 20:38:53 +0200

    small visual tweaks

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Sollvia',
+  title: 'Sollvia',
+  description: 'Your next generation AI tool',
   locale: 'en',
 };
